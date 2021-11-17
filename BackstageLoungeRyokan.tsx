ca920fcf129fde9322177e1ace504e2d8c39f63c
jonas <jonas@rz-k.de>
2021-11-17 02:54:32 +0100

    initial

---
+++ BackstageLoungeRyokan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="BackstageLoungeRyokan" tilewidth="32" tileheight="32" tilecount="270" columns="18">
+ <image source="../rc3-map-lounge/BackstageLoungeRyokan.png" width="576" height="480"/>
+</tileset>
