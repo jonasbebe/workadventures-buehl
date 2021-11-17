bc6a63956648e095ba86fb522a764af83b21ba42
jonas <jonas@rz-k.de>
2021-11-18 00:37:16 +0100

    some bigger map

---
+++ assets/active-cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="active-cave" tilewidth="32" tileheight="32" tilecount="384" columns="24">
+ <image source="../../HSB-Map/active-cave.png" trans="ffffff" width="768" height="512"/>
+</tileset>
---
+++ assets/coco-items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="coco-items" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../../HSB-Map/coco-items.png" trans="ffffff" width="320" height="320"/>
+</tileset>
