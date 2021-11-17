8fbaf921d0862efa0b07beb92a22ceaf35d58be9
jonas <jonas@rz-k.de>
2021-11-17 23:29:07 +0100

    Revert "move hsb"
    This reverts commit 1849ad6fa6d107a8ef4fff0457f52b28e6a73286.

--- FloorTile_S.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="FloorTile_S" tilewidth="32" tileheight="32" tilecount="4488" columns="66">
- <image source="FloorTile_S.jpg" width="2141" height="2180"/>
-</tileset>
