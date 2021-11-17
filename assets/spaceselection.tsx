c2c0613658538916c330b35bd6ea55cacad9343c
jonas <jonas@rz-k.de>
2021-11-17 23:40:59 +0100

    move assets

---
+++ assets/spaceselection.tsx
@@ -0,0 +1,252 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="spaceselection" tilewidth="32" tileheight="32" tilecount="672" columns="28">
+ <image source="spaceselection.png" width="896" height="768"/>
+ <tile id="131">
+  <animation>
+   <frame tileid="47" duration="150"/>
+   <frame tileid="75" duration="150"/>
+   <frame tileid="103" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="162">
+  <animation>
+   <frame tileid="160" duration="100"/>
+   <frame tileid="216" duration="100"/>
+   <frame tileid="272" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="163">
+  <animation>
+   <frame tileid="161" duration="100"/>
+   <frame tileid="217" duration="100"/>
+   <frame tileid="273" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="190">
+  <animation>
+   <frame tileid="188" duration="100"/>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="300" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="191">
+  <animation>
+   <frame tileid="189" duration="100"/>
+   <frame tileid="245" duration="100"/>
+   <frame tileid="301" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="210">
+  <animation>
+   <frame tileid="207" duration="3000"/>
+   <frame tileid="208" duration="500"/>
+   <frame tileid="209" duration="6000"/>
+   <frame tileid="208" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="211">
+  <animation>
+   <frame tileid="123" duration="9999"/>
+   <frame tileid="112" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="212">
+  <animation>
+   <frame tileid="124" duration="9999"/>
+   <frame tileid="113" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="213">
+  <animation>
+   <frame tileid="125" duration="9999"/>
+   <frame tileid="114" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="214">
+  <animation>
+   <frame tileid="126" duration="9999"/>
+   <frame tileid="115" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="238">
+  <animation>
+   <frame tileid="235" duration="3000"/>
+   <frame tileid="236" duration="500"/>
+   <frame tileid="237" duration="6000"/>
+   <frame tileid="236" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="239">
+  <animation>
+   <frame tileid="151" duration="9999"/>
+   <frame tileid="140" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="240">
+  <animation>
+   <frame tileid="152" duration="9999"/>
+   <frame tileid="141" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="241">
+  <animation>
+   <frame tileid="153" duration="9999"/>
+   <frame tileid="142" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="242">
+  <animation>
+   <frame tileid="154" duration="9999"/>
+   <frame tileid="143" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="267">
+  <animation>
+   <frame tileid="179" duration="9999"/>
+   <frame tileid="168" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="268">
+  <animation>
+   <frame tileid="180" duration="9999"/>
+   <frame tileid="169" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="269">
+  <animation>
+   <frame tileid="181" duration="9999"/>
+   <frame tileid="170" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="270">
+  <animation>
+   <frame tileid="182" duration="9999"/>
+   <frame tileid="171" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="282">
+  <animation>
+   <frame tileid="280" duration="100"/>
+   <frame tileid="308" duration="100"/>
+   <frame tileid="336" duration="100"/>
+   <frame tileid="364" duration="100"/>
+   <frame tileid="392" duration="100"/>
+   <frame tileid="420" duration="100"/>
+   <frame tileid="448" duration="100"/>
+   <frame tileid="476" duration="100"/>
+   <frame tileid="504" duration="100"/>
+   <frame tileid="532" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="310">
+  <animation>
+   <frame tileid="281" duration="100"/>
+   <frame tileid="309" duration="100"/>
+   <frame tileid="337" duration="100"/>
+   <frame tileid="365" duration="100"/>
+   <frame tileid="393" duration="100"/>
+   <frame tileid="421" duration="100"/>
+   <frame tileid="449" duration="100"/>
+   <frame tileid="477" duration="100"/>
+   <frame tileid="505" duration="100"/>
+   <frame tileid="533" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="314">
+  <animation>
+   <frame tileid="342" duration="600"/>
+   <frame tileid="370" duration="200"/>
+   <frame tileid="342" duration="1200"/>
+   <frame tileid="370" duration="100"/>
+   <frame tileid="342" duration="600"/>
+   <frame tileid="370" duration="300"/>
+   <frame tileid="342" duration="3200"/>
+   <frame tileid="370" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="315">
+  <animation>
+   <frame tileid="343" duration="600"/>
+   <frame tileid="371" duration="200"/>
+   <frame tileid="343" duration="1200"/>
+   <frame tileid="371" duration="100"/>
+   <frame tileid="343" duration="600"/>
+   <frame tileid="371" duration="300"/>
+   <frame tileid="343" duration="3200"/>
+   <frame tileid="371" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="316">
+  <animation>
+   <frame tileid="344" duration="600"/>
+   <frame tileid="372" duration="200"/>
+   <frame tileid="344" duration="1200"/>
+   <frame tileid="372" duration="100"/>
+   <frame tileid="344" duration="600"/>
+   <frame tileid="372" duration="300"/>
+   <frame tileid="344" duration="3200"/>
+   <frame tileid="372" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="317">
+  <animation>
+   <frame tileid="345" duration="600"/>
+   <frame tileid="373" duration="200"/>
+   <frame tileid="345" duration="1200"/>
+   <frame tileid="373" duration="100"/>
+   <frame tileid="345" duration="600"/>
+   <frame tileid="373" duration="300"/>
+   <frame tileid="345" duration="3200"/>
+   <frame tileid="373" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="318">
+  <animation>
+   <frame tileid="397" duration="400"/>
+   <frame tileid="398" duration="150"/>
+   <frame tileid="399" duration="150"/>
+   <frame tileid="400" duration="150"/>
+   <frame tileid="401" duration="150"/>
+   <frame tileid="397" duration="4000"/>
+  </animation>
+ </tile>
+ <tile id="323">
+  <properties>
+   <property name="openWebsite" value="https://space.bi/stadtrundfahrt"/>
+  </properties>
+ </tile>
+ <tile id="351">
+  <properties>
+   <property name="jitsiRoom" value="spacebi_wohnzimmer"/>
+  </properties>
+ </tile>
+ <tile id="352">
+  <properties>
+   <property name="jitsiRoom" value="spacebi_wc"/>
+  </properties>
+ </tile>
+ <tile id="353">
+  <properties>
+   <property name="jitsiRoom" value="spacebi_wk1"/>
+  </properties>
+ </tile>
+ <tile id="354">
+  <properties>
+   <property name="jitsiRoom" value="spacebi_wk2"/>
+  </properties>
+ </tile>
+ <tile id="532">
+  <animation>
+   <frame tileid="280" duration="100"/>
+   <frame tileid="308" duration="100"/>
+   <frame tileid="336" duration="100"/>
+   <frame tileid="364" duration="100"/>
+   <frame tileid="392" duration="100"/>
+   <frame tileid="420" duration="100"/>
+   <frame tileid="448" duration="100"/>
+   <frame tileid="476" duration="100"/>
+   <frame tileid="504" duration="100"/>
+   <frame tileid="532" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/tilesets_deviant_milkian_1.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tilesets_deviant_milkian_1.png" width="512" height="512"/>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="198">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="214">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="216">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="232">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
