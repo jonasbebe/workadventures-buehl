e316da591c0edf83a1d559a966bb4434f3777cd3
jonas <jonas@rz-k.de>
2021-12-02 01:49:12 +0100

    add some websites

---
+++ assets/CampFire2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="CampFire" tilewidth="32" tileheight="32" tilecount="28" columns="14">
+ <image source="CampFire.png" width="450" height="86"/>
+</tileset>
